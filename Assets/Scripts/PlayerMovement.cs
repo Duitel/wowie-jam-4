1c32c5a5af7caa2465b054eb86ced0e893c966e8
Jop van Roosmalen <jop.van.roosmalen@rws.nl>
2022-08-12 11:53:55 +0200

    Add playermovement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    [SerializeField] private float playerSpeed = 2.0f;
+    [SerializeField] private float turnSpeed = 300f;
+    [SerializeField] private float offsetAngleToAlignWithCamera = 225;
+
+    private void Start()
+    {
+        controller = gameObject.AddComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        move = Quaternion.Euler(0, offsetAngleToAlignWithCamera, 0) * move;
+
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+        if (move != Vector3.zero)
+        {
+            Vector3 turnGap = (move - gameObject.transform.forward);
+            Vector3 turnBy = turnGap.normalized * turnSpeed * Time.deltaTime;
+            if(turnGap.magnitude < turnBy.magnitude)
+            {
+                turnBy = turnGap;
+            }
+
+            gameObject.transform.forward += turnBy;
+        }
+
+        controller.Move(playerVelocity * Time.deltaTime);
+    }
+}

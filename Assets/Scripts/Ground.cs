d4173327cdca590219b16389539b9113e58d05e6
oceull <l.l.vanroosmalen@student.tudelft.nl>
2022-08-13 12:21:57 +0200

    added comments interchanged: "_thisTransform = this.GetCompenent<Transform>()" for "transform"

--- Assets/Scripts/Ground.cs
+++ Assets/Scripts/Ground.cs
@@ -5,40 +5,43 @@ using UnityEngine;
 
 public class Ground : MonoBehaviour
 {
+    // the 3 materials to show the state the block tile is in
     [SerializeField] private Material _normalMaterial;
     [SerializeField] private Material _highlightMaterial;
     [SerializeField] private Material _selectedMaterial;
 
-    private Transform _thisTransform;
+    // used for vertical movemnt of the block tile
     private Vector3 _position;
     private float _startHeight = 5f;
 
+    // the different states for the block tile
     public enum State { NORMAL, HIGHLIGHT, SELECTED };
 
+    // needed to change the material of a GameObject
     private Renderer _renderer;
 
-    // Start is called before the first frame update
+    // Get access to the Material and position of this GameObject
+    // And begin the initiation animation of the block tiles
     void Start()
     {
         _renderer = GetComponent<Renderer>();
-        _thisTransform = this.GetComponent<Transform>();
-        _thisTransform.position = new Vector3(_thisTransform.position.x, -0.5f, _thisTransform.position.z);
-        _position = _thisTransform.position;
+        transform.position = new Vector3(transform.position.x, -0.5f, transform.position.z);
+        _position = transform.position;
         SwitchPosition(_position, Random.Range(0.05f, 0.5f) + 0.01f * _position.z * _position.x);
-        
-
     }
 
+    // start seperate thread for the movement of the block tile
     private void SwitchPosition(Vector3 _targetPosition, float _duration = 0)
     {
         StartCoroutine(SwitchMoving(_targetPosition, _duration));
     }
 
+    // 
     IEnumerator SwitchMoving(Vector3 _targetPosition, float _duration)
     {
-        Vector3 _currentPosisition = _thisTransform.position;
+        Vector3 _currentPosisition = transform.position;
         Vector3 _startPosition = _currentPosisition + Vector3.up * _startHeight;
-        _thisTransform.position = _startPosition;
+        transform.position = _startPosition;
         float _timeElapsed = 0;
         while (_timeElapsed < _duration)
         {
@@ -46,7 +49,7 @@ public class Ground : MonoBehaviour
             _timeElapsed += Time.deltaTime;
             yield return null;
         }
-        _thisTransform.position = _targetPosition;
+        transform.position = _targetPosition;
         SwitchState(State.NORMAL, 0.5f);
     }
 
@@ -67,16 +70,16 @@ public class Ground : MonoBehaviour
         {
             case State.NORMAL:
                 _renderer.sharedMaterial = _normalMaterial;
-                _thisTransform.position = _position;
+                transform.position = _position;
                 break;
             case State.HIGHLIGHT:
                 _renderer.sharedMaterial = _highlightMaterial;
-                _thisTransform.position = _position + Vector3.up * 0.05f;
+                transform.position = _position + Vector3.up * 0.05f;
                 break;
             case State.SELECTED:
                 _renderer.sharedMaterial = _selectedMaterial;
-                
-                _thisTransform.position = _position + Vector3.up * 0.2f;
+
+                transform.position = _position + Vector3.up * 0.2f;
                 break;
         }
     }
